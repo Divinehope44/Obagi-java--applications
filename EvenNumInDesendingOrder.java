44db68dc37dc38665560a8e90fa06aab5af1d72e
Divinehope44 <progresshope97@gmail.com>
2025-04-02 21:51:20 +0100

    Chapter 1 & 2

---
+++ EvenNumInDesendingOrder.java
@@ -0,0 +1,14 @@
+public class EvenNumInDesendingOrder{
+	public static void main(String[] args){
+	
+	int i = 50;
+	
+	while(i >= 1){
+		if(i %2 == 0){
+			System.out.printf("%d%n",1);
+			
+			}
+			1 --;
+		}
+	}
+}
---
+++ WhileLoopStatement.java
@@ -0,0 +1,11 @@
+public class WhileLoopStatement{
+	public static void main(String[] args){
+		
+		int num = 50
+		int i -1;
+		
+		whille (i <= 10){
+			
+
+	}	
+}		
